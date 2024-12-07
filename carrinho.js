22d51cb42986afce967e2811d65e2d65ef09227e
kassime <giovana.kassime.tds2023@gmail.com>
2024-12-07 14:14:54 -0300

    pronto

---
+++ carrinho.js
@@ -0,0 +1,140 @@
+let carrinho = obterTodosProdutos();
+const botaoExcluir = document.querySelectorAll('button');
+
+somaItensCarrinho(carrinho);
+somaValorCarrinho(carrinho);
+
+for (let index = 0; index < carrinho.length; index++) {
+    criarItemCarrinho(carrinho[index]);
+}
+//fiz assim pq n tinha ideia de como pegar cada input q eu cliquei se eles tem id e n entendi nada
+document.querySelectorAll('input[type="number"]').forEach((input, index) => {
+    input.addEventListener('input', (evento) => {
+        carrinho = obterTodosProdutos();
+        const produtoItemNome = document.querySelectorAll('.item-info h2');
+        let nomeProduto = produtoItemNome[index].textContent;
+        let novaQuantidade = parseInt(evento.target.value);//ele pega onde eu cliquei e o valor
+
+        console.log(nomeProduto);
+        carrinho.forEach((produto, indexProduto) => {
+            if (produto.nome === nomeProduto) {
+                if (novaQuantidade === 0) {
+                    carrinho.splice(indexProduto, 1);
+                    const itemCarrinho = evento.target.closest('.item-carrinho');
+                    if (itemCarrinho) {
+                        itemCarrinho.remove();//aqui removea a div inteira
+                    }
+                } else {
+                    produto.quantidade = novaQuantidade;
+                }
+            }
+        });
+        
+        somaItensCarrinho(carrinho);
+        somaValorCarrinho(carrinho);
+        localStorage.setItem('carrinho', JSON.stringify(carrinho));
+    });
+});
+
+function criarItemCarrinho(itemProduto) {
+    //daqui pra baixo fizemos em sala
+    const itemCarrinho = document.createElement('div');
+    itemCarrinho.classList.add('item-carrinho');
+
+    const item = document.createElement('div');
+    item.classList.add('item');
+
+    const itemImg = document.createElement('img');
+    itemImg.src = 'https://picsum.photos/300/500';
+    itemImg.alt = 'Imagem do Produto';
+    itemImg.classList.add('item-img');
+
+    const itemInfo = document.createElement('div');
+    itemInfo.classList.add('item-info');
+
+    const nomeProduto = document.createElement('h2');
+    nomeProduto.textContent = itemProduto.nome;
+
+    const preco = document.createElement('p');
+    preco.classList.add('preco');
+    preco.textContent = itemProduto.preco;
+
+    const botao = document.createElement('button');
+    botao.textContent = 'Remover';
+    botao.classList.add('botao-remover');
+    botao.addEventListener('click', () => {
+        removerItemCarrinho(itemProduto);
+    });
+
+    item.appendChild(itemImg);
+    itemInfo.appendChild(nomeProduto);
+    itemInfo.appendChild(preco);
+    item.appendChild(itemInfo);
+
+    itemCarrinho.appendChild(item);
+    itemCarrinho.appendChild(botao);
+
+    const quantidade = document.createElement('label');
+    quantidade.textContent = 'Quantidade:';
+    const inputQuantidade = document.createElement('input');
+    inputQuantidade.type = 'number';
+    inputQuantidade.id = 'quantidade';
+    inputQuantidade.value = itemProduto.quantidade;
+    inputQuantidade.min = '1';
+    itemInfo.appendChild(quantidade);
+    quantidade.appendChild(inputQuantidade);
+
+    let carrinhoContainer = document.querySelector('.carrinho');
+    carrinhoContainer.prepend(itemCarrinho);//esse prepend aqui ajuda mt pq o total tava ficando em cima de tudp,ai o prepend faz q o item seja add no cmc do carrinho
+}
+
+function removerItemCarrinho(produto) {
+    carrinho = obterTodosProdutos();
+    carrinho = carrinho.filter(item => item.nome !== produto.nome);//aqui ele cria basicamene um novo array só com os produtos que eu n vou excluir, fiz isso por conta do infice q ficava dando problema na hora de excluir e tudo
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+    atualizarCarrinho();
+}
+
+function atualizarCarrinho() {
+    const carrinhoContainer = document.querySelector('.carrinho');
+    carrinhoContainer.innerHTML = ''//limpa tudo e depous ja cria o total dvc e os items pq o indice tava dando projlema ali
+    carrinhoContainer.innerHTML = '<div class="total">' +
+        '<p>Total: R$ 00,00</p>' +
+        '<button class="botao-comprar">Finalizar Compra</button>' +
+        '</div>';
+    
+        carrinho.forEach((itemProduto) => {
+            criarItemCarrinho(itemProduto);
+        });
+        somaItensCarrinho(carrinho);
+        somaValorCarrinho(carrinho);
+}
+    
+
+
+function obterTodosProdutos() {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho'));
+    if (!carrinho) {
+        carrinho = [];
+    }
+    return carrinho;
+}
+
+function somaItensCarrinho(carrinho) {
+    const contador = document.querySelector('.contador');
+    let somaQuantidade = 0;
+    for (let index = 0; index < carrinho.length; index++) {
+        somaQuantidade += carrinho[index].quantidade;
+    }
+    contador.textContent = somaQuantidade;
+}
+
+function somaValorCarrinho(carrinho) {
+    const valor = document.querySelector('.total p');
+    let somaValor = 0;
+    for (let index = 0; index < carrinho.length; index++) {
+        const precoLimpo = parseFloat(carrinho[index].preco.replace('R$', '').trim());//tem que tiear o bgl R$ pra poder calcular
+        somaValor += carrinho[index].quantidade * precoLimpo;
+    }
+    valor.textContent = `R$ ${somaValor.toFixed(2)}`;
+}
--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-
 const botaoAdicionar = document.querySelectorAll('.produto-item a')
 const produtoItemNome = document.querySelectorAll('.produto-item h2')
 const produtoItemPreco = document.querySelectorAll('.produto-item .preco')
-let contador = document.querySelector('.contador')
-
-
+let carrinho = obterTodosProdutos()
+somaItensCarrinho(carrinho)
 
 for (let index = 0; index < 4; index++) {
-    let totais = somarProdutos();
-    contador.innerText = totais.somaQuantidade;
     botaoAdicionar[index].addEventListener('click', (evento) =>{
     evento.preventDefault()//para evitar q o link v apara outra página
-    let carrinho = obterTodosProdutos()
     let produtoExiste = false
     for (let i = 0; i < carrinho.length; i++) {
         let produto = carrinho[i];
@@ -29,63 +24,27 @@ for (let index = 0; index < 4; index++) {
         carrinho.push(produto)
     }
     localStorage.setItem('carrinho', JSON.stringify(carrinho))
-    totais = somarProdutos();
-    contador.innerText = totais.somaQuantidade;
+    somaItensCarrinho(carrinho)
+
     })  
 }
 
-
-
-
-//funções q eu quero reutilizar
-
-function obterTodosProdutos() {
-    let carrinho = JSON.parse(localStorage.getItem('carrinho'));
-    if (carrinho === null || carrinho.length < 1) {
-        carrinho = [];
+function obterTodosProdutos(){
+    let carrinho = JSON.parse(localStorage.getItem('carrinho'))
+    if(carrinho == null || carrinho.length < 1){
+        carrinho = []
     }
-    return carrinho;
+    return carrinho
 }
 
-function atualizarContador() {
-    let totais = somarProdutos();
-    return totais.somaQuantidade;
-}
-
-function adicionarProdutoAoCarrinho(nome, preco) {
-    let carrinho = obterTodosProdutos();
-    let produtoExiste = false;
-
-    for (let i = 0; i < carrinho.length; i++) {
-        let produto = carrinho[i];
-        if (produto.nome === nome) {
-            produto.quantidade++;
-            produtoExiste = true;
-        }
-    }
-
-    if (!produtoExiste) {
-        let produto = {
-            "nome": nome,
-            "preco": preco,
-            "quantidade": 1
-        };
-        carrinho.push(produto);
-    }
-
-    localStorage.setItem('carrinho', JSON.stringify(carrinho));
-}
-function somarProdutos(){
-    let somaPreco = 0
+function somaItensCarrinho(carrinho){
+    const contador = document.querySelector('.contador')
     let somaQuantidade = 0
-    let carrinho = obterTodosProdutos()
-        carrinho.forEach(produto => {
-            let precoNumerico = produto.preco.replace(/[^\d.-]/g, '');//teve que colocar ali pra ele substituir os caracteres, pq o valor fica dando NaN aaaaa peguei no chat pq tava boiando
-            
-            precoNumerico = parseFloat(precoNumerico); // Converte para número
-            somaPreco += precoNumerico * produto.quantidade;
-            somaQuantidade += produto.quantidade;
-        })
-    return {somaPreco, somaQuantidade}
+    for (let index = 0; index < carrinho.length; index++) {
+        somaQuantidade += carrinho[index].quantidade
+    }
+    return contador.textContent = somaQuantidade
 }
+
+
 
